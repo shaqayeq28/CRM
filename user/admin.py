41de9ddebe2bbb4407693f092760c3e08022562c
shaghayegh <valadkhani.sh@gmail.com>
2023-07-25 17:40:56 +0330

    add Lead model to user app

--- user/admin.py
+++ user/admin.py
@@ -1,9 +1,20 @@
 from django.contrib import admin
 
 # Register your models here.
-from user.models import UserProfile
+from user.models import UserProfile, Lead
 
 
 @admin.register(UserProfile)
 class UserProfileAdmin(admin.ModelAdmin):
-    list_display = ['user']
+    list_display = ['user', 'created_time', 'updated_time', 'is_active']
+    readonly_fields = ('created_time', 'updated_time')
+    list_filter = ('is_active',)
+
+
+@admin.register(Lead)
+class LeadAdmin(admin.ModelAdmin):
+    list_display = ['name', 'email', 'priority', 'created_by','status', 'description', 'created_time', 'updated_time',
+                    'is_active']
+    list_filter = ('priority', 'status', 'is_active')
+    raw_id_fields = ('created_by',)
+    readonly_fields = ('created_time', 'updated_time')
--- user/models.py
+++ user/models.py
@@ -2,10 +2,43 @@ from django.contrib.auth.models import User
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
-
 # Create your models here.
 from utils.base_model import BaseModel
 
 
 class UserProfile(BaseModel):
     user = models.OneToOneField(User, related_name='user', on_delete=models.PROTECT, verbose_name=_('user'))
+
+
+class Lead(BaseModel):
+    LOW = 'LOWW'
+    MEDIUM = 'medium'
+    HIGH = 'high'
+
+    PRIORITY_CHOICES = (
+        (LOW, 'low'),
+        (MEDIUM, 'medium'),
+        (HIGH, 'high'),
+    )
+
+    NEW = 'new'
+    CONTACTED = 'contacted'
+    WON = 'won'
+    LOST = 'lost'
+
+    STATUS_CHOICES = (
+        (NEW, 'new'),
+        (CONTACTED, 'contacted'),
+        (WON, 'won'),
+        (LOST, 'LOST'),
+    )
+
+    name = models.CharField(verbose_name=_('name'), max_length=255)
+    email = models.EmailField(verbose_name=_('email'))
+    created_by = models.ForeignKey(User, verbose_name='created by', related_name='leads', on_delete=models.PROTECT)
+    priority = models.CharField(verbose_name=_('priority'), max_length=150, choices=PRIORITY_CHOICES, default=MEDIUM)
+    status = models.CharField(verbose_name=_('status'), max_length=150, choices=STATUS_CHOICES, default=NEW)
+    description = models.TextField(verbose_name=_('description'), blank=True)
+
+    def __str__(self):
+        return f'{self.name} - {self.priority}'
